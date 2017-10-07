acee0a555b16e574b9900e148a803015bc8b2304
Alex Wasinger <alexwasinger@Alexs-MacBook-Pro.local>
2017-10-07 16:10:00 -0500

    request money js

--- donate.js
+++ donate.js
@@ -1,11 +1,17 @@
-var donate = document.getElementById('submit_donation');
+var request_money = document.getElementById('request_money');
+var donate_money = document.getElementById('donate_money');
 
-
-function donation() {
-    var amount = document.getElementById('amount').value;
-    var name = document.getElementById('name').value;
-    alert('Thank you for your donation of $' + amount + ' ' + name + '!');
-    donators[name] = amount
+function request() {
+    var amount = prompt('How much money do you want to request? (No more than $750 allowed)');
+    if (amount > 750) {
+        alert('Please do not take more than $750, leave some money for other clubs.');
+        request();
+    } else if (amount == 0) {
+        alert('Please, take at least some money.');
+        request();
+    } else {
+        alert('Thank you, your request will be reviewed before approval, expect a response within a week.');
+    }
 }
 
-donate.addEventListener('click', donation);
+request_money.onclick = request;
